893ed6f1de645f9eb366662cb77c4bd839260eac
Arda Karagoz <67745458+ardakaragoz@users.noreply.github.com>
2021-03-24 13:01:19 +0300

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,25 @@
+echo off
+color 0f
+title OnlineDB
+cls
+:a
+npm start
+goto a
+
+rem You can also say node index.js
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+rem You Can change title but I suggest to don't touch other things.
